8b4cff954f2e4eeb85fe90bbed8c2beaaccfa676
Jose Medina <fermed28@gmail.com>
2018-02-13 23:17:42 -0500

    se agregan comentarios de lo que falta

--- solution.rb
+++ solution.rb
@@ -1,19 +1,21 @@
 require "sinatra"
 require "make_todo"
+#Falta rutas para update y delete
+#
 
-
-get '/' do
- # if params[:sum]
- #   @sum = params[:sum].to_i + 1
- # else
- #   @sum = 0
- # end
+get '/' do 
 	@tareas= Tarea.all
 	erb :tareas
 end
 
 post '/tareas' do
 	Tarea.create(params[:tarea])
+	@tareas= Tarea.all
+	erb :tareas	
+end
+
+delete 'tareas/:id'
+	Tarea.destroy(params[:id])
 	@tareas= Tarea.all
 	erb :tareas	
 end
